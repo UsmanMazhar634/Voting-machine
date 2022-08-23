0cb94d432e97bd2f0697cb0d921a27d5a305b302
Devsin <dev@Devsins-MacBook-Pro.local>
2022-08-23 23:30:18 +0500

    Added a Welcome Controller

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,10 @@
+# Class for welcome Screen
+class WelcomeController < ApplicationController
+  def welcome_page
+    # Do noting here
+  end
+
+  def dashboard_page
+    # Do noting here
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#index'
+  # root 'users#index'
+  get 'welcome_page', to:'welcome#welcome_page'
+  get 'dashboard_page', to:'welcome#dashboard_page'
+
 end
